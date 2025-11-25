ab3cac4f0bf931c22eef4ef006e8f26aad2da56e
YoussefBedhief <youssefbedhief11@gmail.com>
2025-11-25 12:33:17 +0100

    fix: fixing title in mobile view

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import DropZone from "@/components/DropZone";
 export default function Home() {
   return (
     <div className="max-w-4xl mx-auto flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-4xl font-bold pb-10">Uploading file using S3 📁</h1>
+      <h1 className="text-2xl text-center md:text-4xl font-bold pb-10">
+        Uploading file using S3 📁
+      </h1>
       <DropZone />
     </div>
   );
